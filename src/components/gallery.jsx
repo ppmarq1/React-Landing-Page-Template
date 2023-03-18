4872c5e79be86856deea257d31062f77e5d3b258
Pedro Marques <ppmarq1@gmail.com>
2023-03-18 18:15:22 +0000

    Update data

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -6,10 +6,9 @@ export const Gallery = (props) => {
     <div id="portfolio" className="text-center">
       <div className="container">
         <div className="section-title">
-          <h2>Gallery</h2>
+          <h2>Galería</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+          Nuestras Delicias!
           </p>
         </div>
         <div className="row">
--- src/components/services.jsx
+++ src/components/services.jsx
@@ -5,10 +5,9 @@ export const Services = (props) => {
     <div id="services" className="text-center">
       <div className="container">
         <div className="section-title">
-          <h2>Our Services</h2>
+          <h2>DELIVERY</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+          ¿Quieres disfrutar de un açai en casa o en el trabajo?
           </p>
         </div>
         <div className="row">
