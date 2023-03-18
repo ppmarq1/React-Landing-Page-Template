88ee35ccd8431fa3133c77c5c825409fe263505a
Pedro Marques <ppmarq1@gmail.com>
2023-03-18 15:55:31 +0000

    Update data

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -1,23 +1,33 @@
+import { Image } from "./image";
 import React from "react";
 
 export const Features = (props) => {
   return (
-    <div id="features" className="text-center">
+    <div id="portfolio" className="text-center">
       <div className="container">
-        <div className="col-md-10 col-md-offset-1 section-title">
-          <h2>Features</h2>
+      <div className="section-title">
+      <h2>Menú</h2>
+          <p>
+          ¡Disfrutar!
+          </p>
         </div>
         <div className="row">
-          {props.data
-            ? props.data.map((d, i) => (
-                <div key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
-                  {" "}
-                  <i className={d.icon}></i>
-                  <h3>{d.title}</h3>
-                  <p>{d.text}</p>
-                </div>
-              ))
-            : "Loading..."}
+          <div className="portfolio-items">
+            {props.data
+              ? props.data.map((d, i) => (
+                  <div
+                    key={`${d.title}-${i}`}
+                    className="col-sm-6 col-md-4 col-lg-4"
+                  >
+                    <Image
+                      title={d.title}
+                      largeImage={d.largeImage}
+                      smallImage={d.smallImage}
+                    />
+                  </div>
+                ))
+              : "Loading..."}
+          </div>
         </div>
       </div>
     </div>
