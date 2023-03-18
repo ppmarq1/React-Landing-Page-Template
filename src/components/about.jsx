545b662b7c20acbe5dd70bd90e9c7a90d047c4ce
Pedro Marques <ppmarq1@gmail.com>
2023-03-18 18:24:58 +0000

    Update data

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -7,7 +7,7 @@ export const About = (props) => {
         <div className="row">
           <div className="col-xs-12 col-md-6">
             {" "}
-            <img src="img/Sambo-fruta.png" className="img-responsive" alt="" />{" "}
+            <img src="img/Sambo-fruta.jpg" className="img-responsive" alt="" />{" "}
           </div>
           <div className="col-xs-12 col-md-6">
             <div className="about-text">
