a1ce0284760f028f841f33b2456e3952617a396d
Pedro Marques <ppmarq1@gmail.com>
2023-03-26 15:14:31 +0100

    Add icons

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -32,12 +32,25 @@ export const Gallery = (props) => {
       </div>
         <div id="footer">
           <div className="container text-center">
-            <p>
-              &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
-              <a href="http://www.templatewire.com" rel="nofollow">
-                TemplateWire
+           <li>
+             <a
+                href="https://www.instagram.com/samboacai.es/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src="./Images/instagram.svg" alt="instagram icon" />
               </a>
-            </p>
+            </li>
+            <li>
+              <a
+                href="https://www.facebook.com/samboacai.es"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src="./Images/facebook.svg" alt="facebook icon" />
+              </a>
+            </li>
+            <li></li>
           </div>
         </div> 
     </div>
