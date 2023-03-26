6a59a2648a2ad8eaee7432604f4e64ddbb18bb9a
Pedro Marques <ppmarq1@gmail.com>
2023-03-26 12:51:11 +0100

    Review footer

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -30,6 +30,16 @@ export const Gallery = (props) => {
           </div>
         </div>
       </div>
+        <div id="footer">
+          <div className="container text-center">
+            <p>
+              &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
+              <a href="http://www.templatewire.com" rel="nofollow">
+                TemplateWire
+              </a>
+            </p>
+          </div>
+        </div> 
     </div>
   );
 };
