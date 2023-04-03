3c126aa0465315c7f064e335f9004d82781da747
Pedro Marques <ppmarq1@gmail.com>
2023-04-03 12:02:08 +0100

    Update text

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -33,7 +33,7 @@ export const Gallery = (props) => {
         <div id="footer">
         <div className="container text-center">
           <p>
-            &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
+            &copy; 2023 Pedro Marques React Land Page Template. Design by{" "}
             <a href="http://www.templatewire.com" rel="nofollow">
               TemplateWire
             </a>
